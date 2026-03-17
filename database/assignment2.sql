4a2a9f444b9ad87347311e0db671e1b706229411
Analina Nielsen <analinanielsen@outlook.com>
2026-03-16 22:25:44 -0700

    Add Query 1 (INSERT) tested successfully in pgAdmin

---
+++ database/assignment2.sql
@@ -0,0 +1,3 @@
+-- Query 1: INSERT created and successfully tested in pgAdmin
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
