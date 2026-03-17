d31becbf4e13e5ae3393bbcca44540486c0ce103
Analina Nielsen <analinanielsen@outlook.com>
2026-03-16 22:47:17 -0700

    Add Query 3 (DELETE) tested successfully in pgAdmin

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -2,7 +2,11 @@
 INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
---Query 2: UPDATE tested successfully in pgAdmin (I could have used the email, but I think the ID is the most precise and correct option)
+-- Query 2: UPDATE tested successfully in pgAdmin (I could have used the email, but I think the ID is the most precise and correct option)
 UPDATE account
 SET account_type = 'Admin'
+WHERE account_id = 1;
+
+-- Query 3: DELETE tested successfully in pgAdmin
+DELETE FROM account
 WHERE account_id = 1;
