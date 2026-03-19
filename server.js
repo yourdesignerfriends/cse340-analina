b26b276d93cfdf1824415477b9f0b0b07ecca2a5
Analina Nielsen <analinanielsen@outlook.com>
2026-03-18 22:48:38 -0700

    MVC Getting Started Completed

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -26,9 +27,7 @@ app.use(static)
 /* *******************************
  * Index route
  ******************************* */
-app.get("/", function(req, res){
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
