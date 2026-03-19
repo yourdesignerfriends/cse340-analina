db68b271309bbca9371f3e5acfb4a80b931fd0e7
Analina Nielsen <analinanielsen@outlook.com>
2026-03-19 00:38:14 -0700

    Error Handling Completed

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
+* This server.js file is the primary file of the 
+* application. It is used to control the project.
+*******************************************/
 /* ***********************
- * Require Statements
- *************************/
+* Require Statements
+*************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -12,39 +12,62 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 
 /* ***********************
- * View Engine and Templates
- *************************/
+* View Engine and Templates
+*************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
- * Routes
- *************************/
+* Routes
+*************************/
 app.use(static)
 
 /* *******************************
- * Index route
- ******************************* */
-app.get("/", baseController.buildHome)
+* Index route
+******************************* */
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 /* *******************************
- * Inventory routes
- ******************************* */
+* Inventory routes
+******************************* */
 app.use("/inv", inventoryRoute)
 
+/* *******************************
+* File Not Found Route - must be last route in list
+******************************* */
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
 /* ***********************
- * Local Server Information
- * Values from .env (environment) file
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+/* ***********************
+* Local Server Information
+* Values from .env (environment) file
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
 
 /* ***********************
- * Log statement to confirm server operation
- *************************/
+* Log statement to confirm server operation
+*************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
