ed8ab38db37c820268df86ab930f18b441d36e96
Analina Nielsen <analinanielsen@outlook.com>
2026-03-25 18:13:11 -0700

    Server-Side Data Validation completed

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 /* *******************************
 * Route that handles GET requests for the login page
@@ -17,6 +18,11 @@ router.get("/register", utilities.handleErrors(accountController.buildRegister))
 /* *******************************
 * Process Registration
 ******************************* */
-router.post("/register", utilities.handleErrors(accountController.registerAccount))
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router
