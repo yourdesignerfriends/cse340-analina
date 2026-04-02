68de9462dc05a6d496b7d2d7e59b043252bb378b
Analina Nielsen <analinanielsen@outlook.com>
2026-04-02 13:35:59 -0700

    week 5 task 1 completed

--- utilities/index.js
+++ utilities/index.js
@@ -452,35 +452,37 @@ Util.buildDeleteInventoryForm = function (data = {}) {
 * Middleware to check token validity
 **************************************** */
 Util.checkJWTToken = (req, res, next) => {
- if (req.cookies.jwt) {
-  jwt.verify(
-   req.cookies.jwt,
-   process.env.ACCESS_TOKEN_SECRET,
-   function (err, accountData) {
-    if (err) {
-     req.flash("Please log in")
-     res.clearCookie("jwt")
-     return res.redirect("/account/login")
+    if (req.cookies.jwt) {
+        jwt.verify(
+            req.cookies.jwt,
+            process.env.ACCESS_TOKEN_SECRET,
+            function (err, accountData) {
+                if (err) {
+                    req.flash("Please log in")
+                    res.clearCookie("jwt")
+                    return res.redirect("/account/login")
+                }
+                res.locals.accountData = accountData
+                res.locals.loggedin = 1
+                next()
+            }
+        )
+    }   else {
+        res.locals.loggedin = 0
+        next()
     }
-    res.locals.accountData = accountData
-    res.locals.loggedin = 1
-    next()
-   })
- } else {
-  next()
- }
 }
 
 /* ****************************************
 * Check Login Middleware
 **************************************** */
 Util.checkLogin = (req, res, next) => {
-  if (res.locals.loggedin) {
-    next()
-  } else {
-    req.flash("notice", "Please log in.")
-    return res.redirect("/account/login")
-  }
+    if (res.locals.loggedin) {
+        next()
+    }   else {
+        req.flash("notice", "Please log in.")
+        return res.redirect("/account/login")
+    }
 }
 
 /* ****************************************
